dfce289d67d6294aace4c14718ebafe9508110b1
Dandi <juliandidandi4@gmail.com>
2026-05-07 20:21:13 +0700

    add theme config

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
 
+import { themeConfig } from './configs';
 import { router } from './router';
 
 const queryClient = new QueryClient();
@@ -12,7 +19,10 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} context={{ queryClient }} />
+      <ThemeProvider theme={themeConfig}>
+        <CssBaseline />
+        <RouterProvider router={router} context={{ queryClient }} />
+      </ThemeProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@configs': path.resolve(__dirname, './src/configs'),
       '@components': path.resolve(__dirname, './src/components'),
     },
   },
