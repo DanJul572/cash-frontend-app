dd5f86d79c2176d368f8e17c701296b652bff7c5
Dandi Juliandi <dandi.julaindi@nawadata.com>
2026-05-08 17:31:00 +0700

    add form validation

--- src/features/login/hooks/login-hook.ts
+++ src/features/login/hooks/login-hook.ts
@@ -1,1 +1,22 @@
-export default function useLogin() {}
+import { useForm } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { loginSchema, type LoginFormValues } from '../schemas';
+
+export default function useLogin() {
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: { email: '', password: '' },
+    mode: 'onTouched',
+  });
+
+  const onSubmit = (values: LoginFormValues) => {
+    console.log(values);
+  };
+
+  return {
+    form,
+    onSubmit,
+  };
+}
--- src/features/login/pages/index.tsx
+++ src/features/login/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Controller } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
 import Box from '@mui/material/Box';
@@ -6,28 +7,54 @@ import Card from '@mui/material/Card';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
+import { useLogin } from '../hooks';
 import { cardStyle, containerStyle } from '../styles';
 
 export default function LoginPage() {
   const { t } = useTranslation('login');
+  const { form, onSubmit } = useLogin();
 
   return (
-    <Box sx={containerStyle}>
-      <Card sx={cardStyle}>
-        <Typography variant="h5" component="h2" gutterBottom>
-          Login
-        </Typography>
-        <TextField label={t('username.label')} variant="outlined" fullWidth />
-        <TextField
-          label={t('password.label')}
-          variant="outlined"
-          type="password"
-          fullWidth
-        />
-        <Button variant="outlined" fullWidth>
-          {t('login')}
-        </Button>
-      </Card>
-    </Box>
+    <form onSubmit={form.handleSubmit(onSubmit)} noValidate>
+      <Box sx={containerStyle}>
+        <Card sx={cardStyle}>
+          <Typography variant="h5" component="h2" gutterBottom>
+            Login
+          </Typography>
+          <Controller
+            name="email"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <TextField
+                {...field}
+                label={t('username.label')}
+                variant="outlined"
+                fullWidth
+                error={!!fieldState.error}
+                helperText={fieldState.error?.message}
+              />
+            )}
+          />
+          <Controller
+            name="email"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <TextField
+                {...field}
+                label={t('password.label')}
+                variant="outlined"
+                type="password"
+                fullWidth
+                error={!!fieldState.error}
+                helperText={fieldState.error?.message}
+              />
+            )}
+          />
+          <Button variant="outlined" fullWidth type="submit">
+            {t('login')}
+          </Button>
+        </Card>
+      </Box>
+    </form>
   );
 }
