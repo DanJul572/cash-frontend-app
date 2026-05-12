aa1f6936b5fc2210fdf35e0fa67cad43d4628914
Dandi Juliandi <dandi.julaindi@nawadata.com>
2026-05-12 16:36:18 +0700

    change @fontsource import

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react';
 
 import { createRoot } from 'react-dom/client';
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import { themeConfig } from './configs';
 import { router } from './router';
