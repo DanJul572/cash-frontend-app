0f0867815e1397efc0d7266923b016bee1e2922d
Dandi Juliandi <dandi.julaindi@nawadata.com>
2026-05-19 17:22:31 +0700

    add status code constant

--- src/queries/auth-query.ts
+++ src/queries/auth-query.ts
@@ -1,13 +1,20 @@
 import { queryOptions } from '@tanstack/react-query';
 
+import { STATUS_CODE_CONSTANT } from '@/constants';
 import { authMeRequest } from '@/requests';
 
 export const authMeQuery = queryOptions({
     queryKey: ['auth', 'me'],
     queryFn: async () => {
         const response = await authMeRequest();
-        if (response.status === 401) return null;
-        if (!response.data) throw new Error('Failed to check auth');
+        if (response.status === STATUS_CODE_CONSTANT.UNAUTHORIZED) {
+            return null;
+        }
+
+        if (!response.data) {
+            throw new Error('Failed to check auth');
+        }
+
         return response.data;
     },
     retry: false,
