4a5913791f6871e089cc5ce70eb4b4c805cc9beb
Dandi <juliandidandi4@gmail.com>
2026-05-19 20:44:13 +0700

    styling scroll

--- src/configs/theme-config.ts
+++ src/configs/theme-config.ts
@@ -9,4 +9,28 @@ export const themeConfig = createTheme({
             light: '#FF6D00',
         },
     },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: (theme) => ({
+                '*': {
+                    scrollbarWidth: 'thin',
+                    scrollbarColor: `${theme.palette.grey[400]} transparent`,
+                },
+                '*::-webkit-scrollbar': {
+                    width: '4px',
+                    height: '4px',
+                },
+                '*::-webkit-scrollbar-track': {
+                    background: 'transparent',
+                },
+                '*::-webkit-scrollbar-thumb': {
+                    backgroundColor: theme.palette.grey[400],
+                    borderRadius: '999px',
+                },
+                '*::-webkit-scrollbar-thumb:hover': {
+                    backgroundColor: theme.palette.grey[600],
+                },
+            }),
+        },
+    },
 });
