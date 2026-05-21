d636766787f38627e6d241c0e3e4f1151045514b
Dandi Juliandi <dandi.julaindi@nawadata.com>
2026-05-21 14:44:28 +0700

    remove status and message

--- src/mappers/auth-me-response-mapper.ts
+++ src/mappers/auth-me-response-mapper.ts
@@ -1,8 +1,6 @@
 import { authMeResponseSchema } from '@/schemas';
 
 export const authMeResponseMapper = authMeResponseSchema.transform((res) => ({
-    isSuccess: res.status,
-    message: res.message,
     id: res.data.id,
     name: res.data.name,
     email: res.data.email,
