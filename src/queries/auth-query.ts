117a0e96dbe9ed1653290d7337c8a46dec9a92f0
Dandi Juliandi <dandi.julaindi@nawadata.com>
2026-05-21 13:55:05 +0700

    move logic from query to request

--- src/queries/auth-query.ts
+++ src/queries/auth-query.ts
@@ -1,28 +1,10 @@
 import { queryOptions } from '@tanstack/react-query';
-import axios from 'axios';
 
-import { STATUS_CODE_CONSTANT } from '@/constants';
 import { authMeRequest } from '@/requests';
 
 export const authMeQuery = queryOptions({
     queryKey: ['auth', 'me'],
-    queryFn: async () => {
-        try {
-            const response = await authMeRequest();
-            if (!response.data) {
-                throw new Error('Failed to check auth');
-            }
-            return response.data;
-        } catch (error) {
-            if (
-                axios.isAxiosError(error) &&
-                error.response?.status === STATUS_CODE_CONSTANT.UNAUTHORIZED
-            ) {
-                return null;
-            }
-            throw error;
-        }
-    },
+    queryFn: authMeRequest,
     retry: false,
     staleTime: 1000 * 60 * 5,
 });
--- src/requests/auth-request.ts
+++ src/requests/auth-request.ts
@@ -1,10 +1,22 @@
 import axios from 'axios';
 
+import { STATUS_CODE_CONSTANT } from '@/constants';
 import { AuthEndpoint } from '@/endpoints';
 import type { User } from '@/types';
 
 export const authMeRequest = async () => {
-    return axios.get<User>(AuthEndpoint.me, {
-        withCredentials: true,
-    });
+    try {
+        const response = await axios.get<User>(AuthEndpoint.me, {
+            withCredentials: true,
+        });
+        return response.data;
+    } catch (error) {
+        if (
+            axios.isAxiosError(error) &&
+            error.response?.status === STATUS_CODE_CONSTANT.UNAUTHORIZED
+        ) {
+            return null;
+        }
+        throw error;
+    }
 };
