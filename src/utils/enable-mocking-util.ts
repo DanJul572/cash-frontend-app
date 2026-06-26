3b16c59157a369fc144e35c6175a89304948be8a
Dandi Juliandi <dandi.julaindi@nawadata.com>
2026-06-26 13:18:30 +0700

    remove VITE_ENABLE_MOCKING_REQUEST env

--- src/utils/enable-mocking-util.ts
+++ src/utils/enable-mocking-util.ts
@@ -2,11 +2,9 @@ import { setupWorker } from 'msw/browser';
 
 import { requestMockHandler } from '@mocks';
 
-const enableMockingApiResponse = import.meta.env.VITE_ENABLE_MOCKING_REQUEST === 'true';
 const worker = setupWorker(...requestMockHandler);
 
 const enableMocking = async () => {
-    if (!enableMockingApiResponse) return;
     return worker.start({
         onUnhandledRequest: 'bypass',
     });
