96bb99c11d437010bee29145dce6cd47b9f5339a
Dandi <juliandidandi4@gmail.com>
2026-07-13 19:14:14 +0700

    adjust sidebarContext

--- src/contexts/sidebar-context/sidebar-context.ts
+++ src/contexts/sidebar-context/sidebar-context.ts
@@ -1,8 +1,9 @@
-import { createContext } from 'react';
+import { createContext, type Dispatch, type SetStateAction } from 'react';
 
 export type SidebarContextType = {
     isCollapsed: boolean;
     toggleSidebar: () => void;
+    setIsCollapsed: Dispatch<SetStateAction<boolean>>;
 };
 
 export const SidebarContext = createContext<SidebarContextType | undefined>(undefined);
--- src/contexts/sidebar-context/sidebar-provider.tsx
+++ src/contexts/sidebar-context/sidebar-provider.tsx
@@ -1,13 +1,16 @@
-import { useCallback, useState, type ReactNode } from 'react';
+import { useState, type ReactNode } from 'react';
 
 import { SidebarContext } from './sidebar-context';
 
 export function SidebarProvider({ children }: { children: ReactNode }) {
     const [isCollapsed, setIsCollapsed] = useState(false);
-    const toggleSidebar = useCallback(() => setIsCollapsed((prev) => !prev), []);
+
+    const toggleSidebar = () => {
+        setIsCollapsed((prev) => !prev);
+    };
 
     return (
-        <SidebarContext.Provider value={{ isCollapsed, toggleSidebar }}>
+        <SidebarContext.Provider value={{ isCollapsed, setIsCollapsed, toggleSidebar }}>
             {children}
         </SidebarContext.Provider>
     );
