610a9d35923c32acfe65dedf08d37db83d1c8b88
Dandi <juliandidandi4@gmail.com>
2026-07-28 07:22:15 +0700

    change mocking request label

--- src/configs/request-mock-scenario-config.ts
+++ src/configs/request-mock-scenario-config.ts
@@ -9,8 +9,8 @@ import { authMeRequest401Mock } from '../mocks/auth-request-mock';
 import { guestConfigRequest200Mock } from '../mocks/guest-config-mock';
 
 export const requestMockScenarioConfig: Record<string, HttpHandler[]> = {
-    'auth-me': authMeRequest401Mock,
-    'auth-get-all-alternates': authGetAllAlternates200Mock,
-    'auth-validate-token-alternate': authValidateAlternateToken200Mock,
-    'guest-config': guestConfigRequest200Mock,
+    '/auth/me': authMeRequest401Mock,
+    '/auth/user-alternate/get-users': authGetAllAlternates200Mock,
+    '/auth/user-alternate/validate-token': authValidateAlternateToken200Mock,
+    '/config/guest': guestConfigRequest200Mock,
 };
