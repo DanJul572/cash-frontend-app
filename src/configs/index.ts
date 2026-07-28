f119816fc229fb66b34e4b3136597c739f8156be
Dandi <juliandidandi4@gmail.com>
2026-07-28 07:26:22 +0700

    move mocking config

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,4 +1,3 @@
 export * from './language-config';
-export * from './request-mock-scenario-config';
 export * from './theme-config';
 export * from './tree-menu-config';
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,3 +1,18 @@
-import { requestMockScenarioConfig } from '@configs';
+import type { HttpHandler } from 'msw';
+
+import {
+    authGetAllAlternates200Mock,
+    authValidateAlternateToken200Mock,
+} from '@modules/change-alternate/mocks';
+
+import { authMeRequest401Mock } from './auth-request-mock';
+import { guestConfigRequest200Mock } from './guest-config-mock';
+
+const requestMockScenarioConfig: Record<string, HttpHandler[]> = {
+    '/auth/me': authMeRequest401Mock,
+    '/auth/user-alternate/get-users': authGetAllAlternates200Mock,
+    '/auth/user-alternate/validate-token': authValidateAlternateToken200Mock,
+    '/config/guest': guestConfigRequest200Mock,
+};
 
 export const requestMockHandler = Object.values(requestMockScenarioConfig).flatMap((mock) => mock);
