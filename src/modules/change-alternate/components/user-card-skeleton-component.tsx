e9b9cbd33d571cae2f18a51b0b723aadbb95f9ec
Dandi <juliandidandi4@gmail.com>
2026-08-05 21:07:15 +0700

    fix user card display

--- src/modules/change-alternate/components/user-card-skeleton-component.tsx
+++ src/modules/change-alternate/components/user-card-skeleton-component.tsx
@@ -1,4 +1,3 @@
-import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Skeleton from '@mui/material/Skeleton';
@@ -8,19 +7,12 @@ import { userCardSkeletonComponentStyle } from '../styles';
 const SKELETON_COUNT = 3;
 
 export default function UserCardSkeletonComponent() {
-  return (
-    <Box>
-      {Array.from({ length: SKELETON_COUNT }).map((_, index) => (
-        <Card key={index} sx={userCardSkeletonComponentStyle.cardStyle}>
-          <CardContent sx={userCardSkeletonComponentStyle.cardContentStyle}>
-            <Skeleton variant="circular" sx={userCardSkeletonComponentStyle.avatarSkeletonStyle} />
-            <Skeleton
-              variant="rectangular"
-              sx={userCardSkeletonComponentStyle.buttonSkeletonStyle}
-            />
-          </CardContent>
-        </Card>
-      ))}
-    </Box>
-  );
+  return Array.from({ length: SKELETON_COUNT }).map((_, index) => (
+    <Card key={index} sx={userCardSkeletonComponentStyle.cardStyle}>
+      <CardContent sx={userCardSkeletonComponentStyle.cardContentStyle}>
+        <Skeleton variant="circular" sx={userCardSkeletonComponentStyle.avatarSkeletonStyle} />
+        <Skeleton variant="rectangular" sx={userCardSkeletonComponentStyle.buttonSkeletonStyle} />
+      </CardContent>
+    </Card>
+  ));
 }
