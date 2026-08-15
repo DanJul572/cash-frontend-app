4a74329487bc6988f063058b8458a27a7661ee0d
Dandi <juliandidandi4@gmail.com>
2026-08-15 21:12:49 +0700

    add tooltip in filter button

--- src/components/ztable-filter-toolbar-component.tsx
+++ src/components/ztable-filter-toolbar-component.tsx
@@ -1,9 +1,17 @@
+import { useTranslation } from 'react-i18next';
+
+import { Tooltip } from '@mui/material';
+
 import { FilterPanelTrigger, GridFilterListIcon, ToolbarButton } from '@mui/x-data-grid';
 
 export default function ZTableFilterToolbarComponent() {
+  const { t } = useTranslation('common');
+
   return (
-    <FilterPanelTrigger render={<ToolbarButton />}>
-      <GridFilterListIcon fontSize="small" />
-    </FilterPanelTrigger>
+    <Tooltip title={t('filter')}>
+      <FilterPanelTrigger render={<ToolbarButton />}>
+        <GridFilterListIcon fontSize="small" />
+      </FilterPanelTrigger>
+    </Tooltip>
   );
 }
